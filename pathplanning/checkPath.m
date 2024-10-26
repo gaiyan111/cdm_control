51afa30763ac8f8209be62ca0367a1be3c1e394e
gaiyan111 <gaiyan111@163.com>
2024-10-26 16:20:01 +0800

    Update checkPath.m

--- pathplanning/checkPath.m
+++ pathplanning/checkPath.m
@@ -2,7 +2,7 @@
 function feasible=checkPath(n,newPos,map,circleCenter,r)
 feasible=true;
 movingVec = [newPos(1)-n(1),newPos(2)-n(2),newPos(3)-n(3)];
-movingVec = movingVec/sqrt(sum(movingVec.^2)); %单位化
+movingVec = movingVec/sqrt(sum(movingVec.^2)); 
 for R=0:0.5:sqrt(sum((n-newPos).^2))
     posCheck=n + R .* movingVec;
     if ~(feasiblePoint3(ceil(posCheck),map,circleCenter,r) && feasiblePoint3(floor(posCheck),map,circleCenter,r))
